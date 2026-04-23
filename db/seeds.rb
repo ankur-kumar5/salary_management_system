26bd1c03525de26121ab29bc584716d097523aba
Ankur Kumar <ankursainias@gmail.com>
2026-04-23 22:16:56 +0530

    Added Database Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,22 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# db/seeds.rb
+
+first_names = File.readlines("first_names.txt").map(&:strip)
+last_names  = File.readlines("last_names.txt").map(&:strip)
+
+countries = ["India", "USA", "UK", "Germany"]
+job_titles = ["Engineer", "Manager", "HR", "Analyst"]
+
+employees = []
+
+10000.times do
+  employees << {
+    full_name: "#{first_names.sample} #{last_names.sample}",
+    job_title: job_titles.sample,
+    country: countries.sample,
+    salary: rand(30000..150000),
+    created_at: Time.now,
+    updated_at: Time.now
+  }
+end
+
+Employee.insert_all(employees) # Fast bulk insert
