a8e470aa06174ddb04dd8428f10e04e95d134304
Ankur Kumar <ankursainias@gmail.com>
2026-04-24 16:29:43 +0530

    Introduce pagination

--- Gemfile
+++ Gemfile
@@ -39,7 +39,8 @@ gem "thruster", require: false
 
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
-
+gem 'rack-cors'
+gem 'will_paginate', '~> 3.3'
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri windows ], require: "debug/prelude"
@@ -51,6 +52,7 @@ group :development, :test do
   gem "rubocop-rails-omakase", require: false
 
   gem 'rspec-rails', '~> 8.0.0'
+  gem 'factory_bot_rails'
 end
 
 group :development do
--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,6 +1,21 @@
 class EmployeesController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: [:create, :update, :destroy]
+
+  DEFAULT_PER_PAGE = 20
+  MAX_PER_PAGE = 100
+
   def index
-    render json: Employee.all
+    per_page = sanitized_per_page(params[:per_page])
+    page = params[:page] || 1
+
+    employees = Employee
+                  .order(created_at: :desc)
+                  .paginate(page: page, per_page: per_page)
+
+    render json: {
+      data: employees,
+      meta: pagination_meta(employees)
+    }
   end
 
   def create
@@ -22,5 +37,21 @@ class EmployeesController < ApplicationController
 
   def employee_params
     params.require(:employee).permit(:full_name, :job_title, :country, :salary)
+  end
+
+  def sanitized_per_page(per_page)
+    return DEFAULT_PER_PAGE if per_page.blank?
+
+    [per_page.to_i, MAX_PER_PAGE].min
+  end
+
+  def pagination_meta(collection)
+    {
+      current_page: collection.current_page,
+      next_page: collection.next_page,
+      prev_page: collection.previous_page,
+      total_pages: collection.total_pages,
+      total_count: collection.total_entries
+    }
   end
 end
